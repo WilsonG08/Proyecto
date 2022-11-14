11fddeb03187041e3466e5205996975236eb2a31
Wilson <wilson.guayanay@epn.edu.ec>
2022-11-14 11:24:04 -0500

    mensjeeee

--- src/universidades.java
+++ src/universidades.java
@@ -4,5 +4,6 @@ public class universidades {
         System.out.println("Nombre universidad:" + x.nameUniversity);
         System.out.println("Nombre del estudiante: " + x.nameStudent);
         System.out.println();
+        System.out.println("HOla");
     }
 }
