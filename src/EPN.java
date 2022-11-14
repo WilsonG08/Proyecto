455be2224f132ee2d451bbd8f000028225e62648
marlon <marlon.lalangui@epn.edu.ec>
2022-11-14 10:53:24 -0500

    set and get

--- src/EPN.java
+++ src/EPN.java
@@ -3,4 +3,31 @@ public class EPN {
     String nameUniversity;
     String nameStudent;
     String facultad;
+
+    //metodos
+    public void horarios(){};
+
+    public String getNameUniversity() {
+        return nameUniversity;
+    }
+
+    public void setNameUniversity(String nameUniversity) {
+        this.nameUniversity = nameUniversity;
+    }
+
+    public String getNameStudent() {
+        return nameStudent;
+    }
+
+    public void setNameStudent(String nameStudent) {
+        this.nameStudent = nameStudent;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
 }
